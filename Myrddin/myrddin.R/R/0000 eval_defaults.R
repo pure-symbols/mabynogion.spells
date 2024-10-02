3e606a452ed97836da2fb5f6dbc83e9fa7b4900e
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2024-10-02 16:57:00 +0800

    add package

--- Myrddin/myrddin.R/R/0000 eval_defaults.R
+++ Myrddin/myrddin.R/R/0000 eval_defaults.R
@@ -6,16 +6,16 @@
 #' default_of_basic(lubridate::as_date(999)) # [1] NA
 #' default_of_basic(NA_Date_) # [1] NA
 #' default_of_basic(NA) # [1] FALSE
-#' default_of_basic(default_of_basic (NA_Date_)) # [1] NA
+#' default_of_basic(default_of_basic(NA_Date_)) # [1] NA
 #' default_of_basic(42L) # [1] 0
 #' default_of_basic(666) # [1] 0
 #' default_of_basic(NA_complex_) # [1] 0+0i
 #' default_of_basic(NA_character_) # [1] ""
 #' default_of_basic(NaN) # [1] 0
-#' default_of_basic(logical (0)) # [1] FALSE
-#' default_of_basic(character (0)) # [1] ""
-#' default_of_basic(complex (0)) # [1] 0+0i
-#' default_of_basic(integer (0)) # [1] 0
+#' default_of_basic(logical(0)) # [1] FALSE
+#' default_of_basic(character(0)) # [1] ""
+#' default_of_basic(complex(0)) # [1] 0+0i
+#' default_of_basic(integer(0)) # [1] 0
 #' 
 default_of_basic = function (.val, .len = 1) .val |> 
 	base::class() |> 
