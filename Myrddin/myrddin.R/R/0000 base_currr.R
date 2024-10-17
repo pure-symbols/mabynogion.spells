a8ed9427a150e4ac606440aa419f160c69d5a35c
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2024-10-17 08:11:06 +0800

    Update 0000 base_currr.R

--- Myrddin/myrddin.R/R/0000 base_currr.R
+++ Myrddin/myrddin.R/R/0000 base_currr.R
@@ -1,5 +1,7 @@
 
 currr_pair = \ (a) \ (b) \ (c) c (a) (b)
 currr_tuple = \ (n, p = base::list()) if 
-	(n > 0) \ (...) currr_tuple (n - 1, p |> list_append(...)) else if 
-	(T) \ (f) p |> looper_reduce(fn_apply, .init = f)
+	(n != 0) \ (...) (
+		\ (..param_concater) currr_tuple (n - n_pom(n), p |> ..param_concater(...))) (
+		..param_concater = if (n_lpom(n)) list_append else list_follow) else if 
+	(n == 0) \ (f) p |> looper_reduce(fn_apply, .init = f)
