2c437b8b1fa6303c953c5137b742d1148616eaa2
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2024-10-22 14:47:29 +0800

    Update 0000 base_func.R

--- Myrddin/myrddin.R/R/0000 base_func.R
+++ Myrddin/myrddin.R/R/0000 base_func.R
@@ -1,7 +1,7 @@
 
 #' Bind function
 #' 
-`%bind%` = function (f.a, f.b) \ (...) f.b(f.a(...))
+`%bind%` = `%.%` = function (f.a, f.b) \ (...) f.b(f.a(...))
 
 binder = function (..., .fns = base::list(...)) .fns |> looper_reduce(`%bind%`)
 
