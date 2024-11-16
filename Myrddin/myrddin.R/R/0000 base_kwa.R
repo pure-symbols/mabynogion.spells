a01cbbcdca3c7ac2a62cebe6c6de92906deb7880
Ypa Y Yhm <y.ypa.yhm@skiff.com>
2024-11-16 09:28:38 +0800

    - kwa - kount - c -

--- Myrddin/myrddin.R/R/0000 base_kwa.R
+++ Myrddin/myrddin.R/R/0000 base_kwa.R
@@ -3,17 +3,19 @@
 
 kount = function (
 		nums, 
-		.base = base::length(nums) * (.by * 2), 
-		.by = 1, 
+		.base = base::length(nums) * (.step_by * 2), 
+		.step_by = 1, 
 		.unbase = F) (nums) |> 
 	looper_reduce(
-		.f = \ (a, b) base::seq.int(from = a$n, length.out = b, by = .by) |> 
+		.f = \ (a, b) base::seq.int(from = a$n, length.out = b, by = .step_by) |> 
 			magrittr::'%>%'({base::list(
 				m = a$m |> list_append(..pairs = b |> list_pairs(.)), 
+				c = a$c |> base::c(.[base::length(.)]), 
 				n = .[base::length(.)])}),
-		.init = base::list(m = base::list(), n = 0)) |> 
+		.init = base::list(m = base::list(), n = 0, c = base::c())) |> 
 	(if (.unbase) base::identity else \ (.) base::list(
 		m = .$m |> base::lapply(\ (x) x %% .base), 
+		c = .$c %% .base, 
 		n = .$n %% .base))() |> 
 	base::identity()
 
@@ -30,6 +32,9 @@ kount = function (
 #| [1] 1 2 3 4 5 0 1 2 3
 #| 
 #| 
+#| $c
+#| [1] 2 1 3
+#| 
 #| $n
 #| [1] 3
 #| 
@@ -44,6 +49,9 @@ kount = function (
 #| $m$`9`
 #| [1]  7  8  9 10 11 12 13 14 15
 #| 
+#| 
+#| $c
+#| [1]  2  7 15
 #| 
 #| $n
 #| [1] 15
